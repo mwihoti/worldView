7dead41f29f5662a921783c0069b79b52f67be68
mwihoti <danielmwihoti@gmail.com>
2025-01-07 16:03:44 +0300

    Feat fix navbar height on mobile device

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import ThemeToggler from "./theme-toggler";
 import { Button } from "./ui/button";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
-import { getBlogName } from "@/lib/requests";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faTimes, faFutbol, faFilm, faLaptopCode } from "@fortawesome/free-solid-svg-icons";
@@ -16,13 +15,15 @@ export default function Navbar() {
 
   const toggleMenu = () => setMenuOpen(!menuOpen);
 
-  const title = { displayTitle: "World View" }; // Replace with a proper call to `getBlogName` if needed in a client-safe way.
+  const closeMenu = () => setMenuOpen(false);
+
+  const title = { displayTitle: "World View" };
 
   return (
     <div className="w-full border-b mb-5 bg-white shadow-md">
-      <div className="max-w-7xl w-full px-5 xl:px-0 mx-auto flex justify-between items-center py-4">
+      <div className="max-w-7xl w-full px-5 xl:px-0 mx-auto flex justify-between items-center py-2">
         {/* Blog Title */}
-        <div className="text-2xl font-bold">
+        <div className="text-xl font-bold">
           <Link href="/">{title.displayTitle || "My Blog"}</Link>
         </div>
 
@@ -36,26 +37,37 @@ export default function Navbar() {
 
         {/* Navigation Links */}
         <div
-          className={`flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-8 fixed lg:static top-0 left-0 w-full lg:w-auto h-screen lg:h-auto bg-white lg:bg-transparent transition-transform transform ${
+          className={`fixed lg:static top-0 left-0 w-full lg:w-auto h-[50vh] lg:h-auto bg-white lg:bg-transparent transition-transform transform ${
             menuOpen ? "translate-x-0" : "-translate-x-full"
-          } lg:translate-x-0`}
+          } lg:translate-x-0 z-10`}
         >
-          <nav className="flex flex-col lg:flex-row items-center gap-5 lg:gap-8 p-6 lg:p-0">
+          {/* Close menu overlay */}
+          {menuOpen && (
+            <div
+              onClick={closeMenu}
+              className="fixed top-0 left-0 w-full h-screen bg-black bg-opacity-50 lg:hidden"
+            />
+          )}
+
+          <nav className="flex flex-col lg:flex-row items-center gap-5 lg:gap-8 p-4 lg:p-0 relative z-20">
             <Link
               href="/posts?author=Dennis"
               className="text-lg flex items-center gap-2 hover:text-blue-500"
+              onClick={closeMenu}
             >
               <FontAwesomeIcon icon={faFutbol} /> Sports
             </Link>
             <Link
               href="/posts?author=Danny"
               className="text-lg flex items-center gap-2 hover:text-blue-500"
+              onClick={closeMenu}
             >
               <FontAwesomeIcon icon={faFilm} /> Movies & TV Shows
             </Link>
             <Link
               href="/posts?author=Daniel"
               className="text-lg flex items-center gap-2 hover:text-blue-500"
+              onClick={closeMenu}
             >
               <FontAwesomeIcon icon={faLaptopCode} /> Tech
             </Link>
